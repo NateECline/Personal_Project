82acf725c7693797394b2936cdae6ad16b638712
NateCline <106636129+NateCline@users.noreply.github.com>
2022-07-08 22:59:29 -0600

    nothing

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Nothing")
+class Card {
+    constructor(suit, rank, value){
+    this.suit = suit;
+    this.rank = rank;
+    this.value = value;
+    }
+  }
+  
+  class Deck() {
+    constructor() {
+      this.deckOfCards = [];
+      this.createHand();
+      }
+      
+        createHand() {
+         const suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+         let ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
+         let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
+  
+         for (let i = 0; i < suits.length; i++) {
+          for (let r = 0; r < ranks.length; r++) {
+            this.deckOfCards.push(new Card(suits[i], ranks[r], values[r]));
+          }
+         }
+        }
+      }
+  
+      let newDeck = newDeck();
+      console.log(newDeck)
